7299f4afe3efd300d0e54af56c9523e9cc3a01ef
cybooo <info@cyborggg.eu>
2022-07-05 20:06:30 +0200

    Add missing annotations

--- src/main/java/com/github/cybooo/skyblock/commands/IslandCommand.java
+++ src/main/java/com/github/cybooo/skyblock/commands/IslandCommand.java
@@ -11,6 +11,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import java.time.Duration;
 import java.util.concurrent.ConcurrentMap;
@@ -32,7 +33,7 @@ public class IslandCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         if (!(sender instanceof Player player)) {
             sender.sendMessage("Tento prikaz neni dostupny z konzole.");
             return false;
